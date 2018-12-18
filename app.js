c94fa5d1095a4ded66cc7bfc325a0004c9519483
shaheenrumi <Shaheenrumi@uconnect.pk>
2018-12-18 17:56:56 +0500

    mini project

---
+++ app.js
@@ -0,0 +1,73 @@
+// let i = null;
+// while(i<5){
+//   alert(i)
+//   i++;
+// }
+
+// let i = null;
+// while(i>5){
+//   alert(i)
+//   i++;
+// }
+// alert("error");
+
+// let i = 0;
+// while(i<5){
+//   alert(i);
+//   i++;
+// }
+// let i = undefined;
+// while(i<5){
+//   alert(i);
+//   i++;
+// }
+// alert('hi');
+// let i = undefined;
+// while(i==null){
+//   alert(i);
+//   i++;
+// }
+// alert('hi');
+
+// let i = NaN;
+// while(i==0){
+//   alert(i);
+//   i++;
+// }
+// alert('hi');
+// let i = "hi";
+// while(i<"hii"){
+//   alert(i);
+//   i++;
+// }
+// let i = 0, j=10;
+// while(i<=10){
+//  console.log(j+ "*" +i+ " = " +j*i );
+//   i++;
+// }
+
+// let i = 0;
+// while(i<5){
+//   alert(i);
+
+// }
+// let i = 5;
+// while(i<10){
+//   alert(i);
+//   i--
+// }
+// let i = 5;
+// while(i>0){
+//   alert(i);
+//   i--
+// }
+// let i = 4;
+// while(i){
+//   alert(i)
+//   i;
+// }
+let i = 4;
+while(i<8){
+  alert(++ i);
+}
+
